37698df5c348880cf8926a9b54838c75ae98fd38
Ross <rosso5792@gmail.com>
2020-01-10 13:55:36 +0000

    mvp complete, time to style and work on extensions

--- js/app.js
+++ js/app.js
@@ -3,14 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 const form = document.querySelector('#new-form')
 form.addEventListener('submit', handleFormSubmit)
 
-
-
-
-
-
-
-
-
+const deleteButton = document.querySelector('.delete-btn')
+deleteButton.addEventListener('click', handleDeleteButtonOnClick)
 
 });
 
@@ -18,4 +12,24 @@ form.addEventListener('submit', handleFormSubmit)
 const handleFormSubmit = (event) => {
   event.preventDefault()
 
+  const newCountryItem = document.createElement('li')
+  newCountryItem.textContent = `Country: ${event.target.country.value}`
+
+  const newContinentItem = document.createElement('li')
+  newContinentItem.textContent = `Contitent: ${event.target.continent.value}`
+
+  const newDateOfTravelItem = document.createElement('li')
+  newDateOfTravelItem.textContent = `Date of travel: ${event.target.dateOfTravel.value}`
+
+  const list = document.querySelector('#trip-list')
+  list.appendChild(newCountryItem)
+  list.appendChild(newContinentItem)
+  list.appendChild(newDateOfTravelItem)
+
+  event.target.reset()
+}
+
+const handleDeleteButtonOnClick = () => {
+  const clearList = document.querySelector('#trip-list')
+  clearList.innerHTML = "";
 }
